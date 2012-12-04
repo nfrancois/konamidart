fad81fa195942bd23399130021de4ac4170b7f4b
Nicolas FRANCOIS <nicolas.franc@gmail.com>
2012-12-04 15:50:51 +0100

    multi listener

--- example/web/sample.dart
+++ example/web/sample.dart
@@ -4,8 +4,7 @@ import 'package:konami_code/konami_code.dart';
 
 
 main(){
-  var konami = new KonamiCode();
-  konami.onPerformed = _changeText;
+  konamiCode.on.performed.add(_changeText);
 }
 
 _changeText(){
--- lib/konami_code.dart
+++ lib/konami_code.dart
@@ -2,33 +2,97 @@ library konami_code;
 
 import 'dart:html';
 
-class KonamiCode {
+
+KonamiCode _konamiCode;
+
+/// Get access to konami code.
+KonamiCode get konamiCode {
+  if(_konamiCode == null){
+    _konamiCode = new _KonamiCodeImpl();
+  }
+  return _konamiCode;
+}
+
+/**
+ * Encapsulates Konami code execution by listening keyboard.
+ */
+abstract class KonamiCode {
+
+  KonamiCodeEvents get on;
   
-  const KONAMICODE_SEQUENCE = "38384040373937396665";
+}
+
+/**
+ * Konami code events.
+ */ 
+class KonamiCodeEvents {
+ 
+  final KonamiCodeEventListenerList _onPerformedEvents;
   
-  var _onPerformedCallback;
+  KonamiCodeEvents() : _onPerformedEvents = new KonamiCodeEventListenerList();
+  
+  KonamiCodeEventListenerList get performed => _onPerformedEvents;
+ 
+}
+
+/**
+ * Encapsulate all Konami code listeners
+ */ 
+class KonamiCodeEventListenerList {
+  
+  List<KonamiCodeHandler> _listeners; 
+  
+  KonamiCodeEventListenerList(){
+    _listeners = [];
+  }
+  
+  /**
+   * Add a Konamide code hander to listener list.
+   */ 
+  add(KonamiCodeHandler handler) {
+    _listeners.add(handler);
+  }
+  
+  _dispatch(){
+    _listeners.forEach((handler) => handler());
+  }
+  
+}
+
+/**
+ * Call when Konami code is performed.
+ */ 
+typedef void KonamiCodeHandler();
+
+
+class _KonamiCodeImpl implements KonamiCode {
+  
+  /// Konami code execution
+  static const _KONAMICODE_SEQUENCE = "38384040373937396665";
+  
+  final KonamiCodeEvents _events;
   String _lastInputs;
   
-  KonamiCode(){
+  _KonamiCodeImpl() :  _events = new KonamiCodeEvents() {
     _lastInputs = "";
+    _bind();
+  }
+  
+  _bind(){
     window.on.keyDown.add(_onKeyDown);
   }
   
   _onKeyDown(KeyboardEvent event){
     var keyCode = event.keyCode;
     _lastInputs = _lastInputs.concat(keyCode.toString());
-    if(KONAMICODE_SEQUENCE == _lastInputs){
+    if(_KONAMICODE_SEQUENCE == _lastInputs){
       _lastInputs = "";
-      if(_onPerformedCallback != null){
-        _onPerformedCallback();        
-      }
-    } else if(_lastInputs.length > KONAMICODE_SEQUENCE.length){
-      _lastInputs = _lastInputs.substring(_lastInputs.length-KONAMICODE_SEQUENCE.length);
+      on.performed._dispatch();
+    } else if(_lastInputs.length > _KONAMICODE_SEQUENCE.length){
+      _lastInputs = _lastInputs.substring(_lastInputs.length-_KONAMICODE_SEQUENCE.length);
     }
   }
   
-  void set onPerformed(callback()) {
-    _onPerformedCallback = callback;
-  }
+ KonamiCodeEvents get on => _events;
   
 }
