d25e4e02a59020f217759433642ed5eefdfaa0e3
Nicolas FRANCOIS <nicolas.franc@gmail.com>
2012-12-04 17:14:54 +0100

    add license

--- lib/konami_code.dart
+++ lib/konami_code.dart
@@ -1,3 +1,11 @@
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * @nicofrancois wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return Nicolas François
+ * ----------------------------------------------------------------------------
+ */
 library konami_code;
 
 import 'dart:html';
