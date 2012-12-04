890ca0b07f133a67c4e4820c388d62f15d7888b2
Nicolas FRANCOIS <nicolas.franc@gmail.com>
2012-12-04 17:06:32 +0100

    indentation

--- lib/konami_code.dart
+++ lib/konami_code.dart
@@ -14,15 +14,15 @@ KonamiCode get konamiCode {
 }
 
 /**
- * A Konami Code event. 
+ * A Konami Code event.
  */
 class KonamiCodeEvent {
-  
+
   final String type;
-  
+
   KonamiCodeEvent.performed() : type = "performed";
-  
-  String toString() => type;  
+
+  String toString() => type;
 }
 
 /**
@@ -34,66 +34,66 @@ abstract class KonamiCode {
    * Provide events.
    */
   KonamiCodeEvents get on;
-  
+
 }
 
 /**
  * Provides access to Konami Code events listeners.
- */ 
+ */
 class KonamiCodeEvents {
- 
+
   final KonamiCodeEventListenerList _onPerformedEvents;
-  
+
   KonamiCodeEvents() : _onPerformedEvents = new KonamiCodeEventListenerList();
-  
+
   /**
    * Call when Konami Code is performed.
    */
   KonamiCodeEventListenerList get performed => _onPerformedEvents;
- 
+
 }
 
 /**
  * Encapsulate all Konami Code listeners.
- */ 
+ */
 class KonamiCodeEventListenerList {
-  
-  final Set<KonamiCodeListener> _listeners; 
-  
+
+  final Set<KonamiCodeListener> _listeners;
+
   KonamiCodeEventListenerList() : _listeners = new Set<KonamiCodeListener>();
-  
+
   /**
    * Add a Konamide Code listener to listener list.
-   */ 
+   */
   add(KonamiCodeListener listener) => _listeners.add(listener);
-  
+
   remove(KonamiCodeListener listener) => _listeners.remove(listener);
-  
+
   _dispatch(KonamiCodeEvent event) => _listeners.forEach((listener) => listener(event));
-  
+
 }
 
 /**
  * Call when Konami Code is performed.
- */ 
+ */
 typedef void KonamiCodeListener(KonamiCodeEvent event);
 
 
 class _KonamiCodeImpl implements KonamiCode {
-  
+
   /// Konami Code ascii sequence
   static const _KONAMICODE_SEQUENCE = "38384040373937396665";
-  
+
   final KonamiCodeEvents _events;
   String _lastInputs;
-  
+
   _KonamiCodeImpl() :  _events = new KonamiCodeEvents() {
     _lastInputs = "";
     _bind();
   }
-  
+
   _bind() => window.on.keyDown.add(_onKeyDown);
-  
+
   _onKeyDown(KeyboardEvent event){
     var keyCode = event.keyCode;
     _lastInputs = _lastInputs.concat(keyCode.toString());
@@ -104,7 +104,7 @@ class _KonamiCodeImpl implements KonamiCode {
       _lastInputs = _lastInputs.substring(_lastInputs.length-_KONAMICODE_SEQUENCE.length);
     }
   }
-  
- KonamiCodeEvents get on => _events;
-  
+
+  KonamiCodeEvents get on => _events;
+
 }
