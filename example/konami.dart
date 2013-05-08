e776196e7e23c200476631ce51a498693b51500a
Nicolas FRANCOIS <nicolas.franc@gmail.com>
2013-05-08 20:40:18 +0200

    beerware licence

--- example/konami.dart
+++ example/konami.dart
@@ -1,3 +1,12 @@
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * Nicolas Francois wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return Nicolas François
+ * ----------------------------------------------------------------------------
+ */
+
 import "package:konami_code/konami_code.dart";
 import "dart:html";
 
